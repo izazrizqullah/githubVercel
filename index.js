b1ff808668cba832d709432e1b19630400780dc4
izazrizqullah <izazrizqullah@gmail.com>
2022-10-19 19:54:30 +0700

    update: add morgan

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const morgan = require("morgan");
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.status.json({
